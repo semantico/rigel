316e5dcf3751409aedb8ade19bcac3393cff0d8a
Edd King <edd.king@semantico.com>
2013-10-01 13:06:19 +0100

    implemented content repo factory

--- src/main/java/com/semantico/rigel/ContentRepositoryFactory.java
+++ src/main/java/com/semantico/rigel/ContentRepositoryFactory.java
@@ -2,17 +2,33 @@ package com.semantico.rigel;
 
 import java.util.Map;
 
+import org.apache.solr.client.solrj.SolrRequest.METHOD;
+import org.apache.solr.client.solrj.SolrServer;
+
 import com.google.common.collect.Maps;
 
 public class ContentRepositoryFactory {
 
     private final Map<ContentItem.Schema<?>, ContentRepository<?>> singletons;
 
-    public ContentRepositoryFactory() {
-        singletons = Maps.newHashMap();
+    private final ContentItemFactory contentItemFactory;
+    private final SolrServer server;
+    private final METHOD method;
+
+    public ContentRepositoryFactory(SolrServer server, METHOD method, ContentItemFactory contentItemFactory) {
+        this.server = server;
+        this.method = method;
+        this.contentItemFactory = contentItemFactory;
+        this.singletons = Maps.newHashMap();
     }
 
-    public <T extends ContentItem> ContentRepository<T> getRepository(ContentItem.Schema<T> schema) {
-        return null;
+    public synchronized <T extends ContentItem> ContentRepository<T> getRepository(ContentItem.Schema<T> schema) {
+        ContentRepository<T> repo = (ContentRepository<T>) singletons.get(schema);
+        if (repo == null) {
+            repo = new ContentRepositoryImpl<T>(server, contentItemFactory, method, schema);
+            singletons.put(schema, repo);
+        }
+        return repo;
     }
+
 }
